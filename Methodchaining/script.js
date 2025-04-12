040df99cbc6dd79ba5fb04a867c9e9a106de726c
Kwamz3 <charlie18appiah@gmail.com>
2025-04-12 05:12:14 +0000

    extrachar

--- Methodchaining/script.js
+++ Methodchaining/script.js
@@ -1,22 +1,22 @@
 
-// let username = window.prompt("Enter your name: ");
-// username = username.trim();
+let username = window.prompt("Enter your name: ");
+username = username.trim();
 
-// let letter = username.charAt(0);
-// letter = letter.toUpperCase()
+let letter = username.charAt(0);
+letter = letter.toUpperCase()
 
-// let extraChars = username.slice(1)
-// extraChars = extraChars.toLowerCase()
+let extraChars = username.slice(1)
+extraChars = extraChars.toLowerCase()
 
-// username = letter + extraChars  
-// console.log(username);
+username = letter + extraChars  
+console.log(username);
 
 
 // Method chaining
 
-let username = window.prompt("Enter your name: ");
+// let username = window.prompt("Enter your name: ");
 
-username = username.trim().charAt(0).toUpperCase() +
-    username.slice(1).toLowerCase()
+// username = username.trim().charAt(0).toUpperCase() +
+//     username.slice(1).toLowerCase()
 
-console.log(username);
+// console.log(username);
