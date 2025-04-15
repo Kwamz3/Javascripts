00a26cedea3f46e5b784a935d25c02a7a5896495
Kwamz3 <charlie18appiah@gmail.com>
2025-04-15 00:04:17 +0000

    array

--- arrayscript/script.js
+++ arrayscript/script.js
@@ -1,14 +1,15 @@
 let fruits = ["apple", "banana", "orange", "coconut", "pear"];
 
-// fruits.push("guava");
 // fruits.pop();
 // fruits.unshift("mango");
+// fruits.push("guava");
 
 // console.log(fruits[0]);
 // console.log(fruits[1]);
 // console.log(fruits[2]);
 // console.log(fruits[3]);
 // console.log(fruits[4]);
+// console.log(fruits[5]);
 
 // let index = fruits.indexOf("banana")
 // let numOFArray = fruits.length
@@ -22,4 +23,3 @@ fruits.sort().reverse();
 for (let fruit of fruits){
     console.log(fruit);
 }
-
