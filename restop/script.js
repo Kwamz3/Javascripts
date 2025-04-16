898e838dbaadfa4781b36ac02f494922e93beb34
Kwamz3 <charlie18appiah@gmail.com>
2025-04-16 12:04:04 +0000

    restop

--- restop/script.js
+++ restop/script.js
@@ -1,6 +1,24 @@
-function tableTop(...stuff) {
-    console.log(stuff);
+// function commonsEye(...things) {
+//     console.log(things);
+// }
+
+
+// let thing1 = "computers";
+// let thing2 = "book";
+// let thing3 = "phones";
+// let thing4 = "pods";
+// let thing5 = "mouse";
+
+// commonsEye(thing1, thing2, thing3, thing4, thing5)
+
+function commonsEye(...things) {
+    console.log(...things);
 }
 
-let tabletop1 = "perfume"; 
-tableTop("brush", "comb", "pomade", "sure", tabletop1)
+let thing1 = "computers";
+let thing2 = "book";
+let thing3 = "phones";
+let thing4 = "pods";
+let thing5 = "mouse";
+
+commonsEye(thing1, thing2, thing3, thing4, thing5)
