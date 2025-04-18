bd790ee68b766da8bf384b99b965cc6b1ce9c971
Kwamz3 <charlie18appiah@gmail.com>
2025-04-18 18:07:40 +0000

    restop

--- restop/script.js
+++ restop/script.js
@@ -11,14 +11,22 @@
 
 // commonsEye(thing1, thing2, thing3, thing4, thing5)
 
+// function commonsEye(...things) {
+//     console.log(...things);
+// }
+
+
+
 function commonsEye(...things) {
-    console.log(...things);
+    return foods
 }
 
-let thing1 = "computers";
-let thing2 = "book";
-let thing3 = "phones";
-let thing4 = "pods";
-let thing5 = "mouse";
+const thing1 = "computers";
+const thing2 = "book";
+const thing3 = "phones";
+const thing4 = "pods";
+const thing5 = "mouse";
 
-commonsEye(thing1, thing2, thing3, thing4, thing5)
+const thing = commonsEye(thing1, thing2, thing3, thing4, thing5);
+
+console.log(thing);
