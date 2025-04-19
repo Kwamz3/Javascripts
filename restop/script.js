19c24f20f6e37fb4c6c9797ba0ba18f823cc7c0a
Kwamz3 <charlie18appiah@gmail.com>
2025-04-19 15:47:28 +0000

    restop

--- restop/script.js
+++ restop/script.js
@@ -15,16 +15,42 @@
 //     console.log(...things);
 // }
 
-function commonsEye(...things) {
-    return things
+// function commonsEye(...things) {
+//     return things
+// }
+
+// let thing1 = "book"
+// let thing2 = "mouse"
+// let thing3 = "phone"
+// let thing4 = "pods"
+// let thing5 = "laptop"
+
+// const thing = commonsEye(thing1, thing2, thing3, thing4, thing5);
+// console.log(thing);
+
+// function sumFunc(...numbers) {
+//     let result = 0;
+//     for (let number of numbers) {
+//         result += number;
+//     }
+//     return result;
+// }
+
+// const total = sumFunc(3, 5, 56, 79, 89, 46, 69, 56);
+// console.log(`Your total is $${total}`);
+
+// function getAverage(...numbers) {
+//     let result = 0;
+//     for (let number of numbers) {
+//         result += number;
+//     }
+//     return result / numbers.length;
+// }
+
+// const total = getAverage(30, 80, 97, 65, 72);
+// console.log(total);
+
+function combineStrings(...string) {
+    console.log(...string);
 }
-
-let thing1 = "book"
-let thing2 = "mouse"
-let thing3 = "phone"
-let thing4 = "pods"
-let thing5 = "laptop"
-
-const thing = commonsEye(thing1, thing2, thing3, thing4, thing5);
-console.log(thing);
-
+const fullName = combineStrings("Mr.", "SpongeBob", "SquarePants", "III");
