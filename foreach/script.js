b9d3cdefb7f124e70124c454e8087564587d97f9
Kwamz3 <charlie18appiah@gmail.com>
2025-05-05 23:18:44 +0000

    element

--- foreach/script.js
+++ foreach/script.js
@@ -1,7 +1,18 @@
+let array = [1, 2, 3, 4, 5];
 
-let numbers = [1, 2, 3, 4, 5];
+array.forEach(display);
 
-numbers.forEach(displayElement);
-function displayElement(element){
+function double(element, index, array) {
+    array[index] = element * 2
+}
+
+function display(element) {
     console.log(element)
 }
+
+array.forEach(double);
+function double(element) {
+    array = element * 2;
+    console.log(array);
+}
+
