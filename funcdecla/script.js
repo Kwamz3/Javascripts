8dab681a97a73d370780fd95d1b90754e5ec40cf
Kwamz3 <charlie18appiah@gmail.com>
2025-05-15 21:52:37 +0000

    funcdeclaration

--- funcdecla/script.js
+++ funcdecla/script.js
@@ -4,18 +4,17 @@
 
 // hello();
 
-function cardScan(callback, delay) {
-    console.log("Scanning...");
+// function cardScan(callback, delay) {
+//     console.log("Scanning...");
 
-    setTimeout(function () {
-        callback();
-    }, delay);
+//     setTimeout(function () {
+//         callback();
+//     }, delay);
+// };
 
-};
-
-cardScan(function () {
-    console.log("Scan completed!")
-}, 3000)
+// cardScan(function () {
+//     console.log("Approved!...yeeeeyaaaaahhhhhh")
+// }, 3000);
 
 // function cardScan(callback, delay) {
 //     console.log("Scanning card...");
@@ -28,3 +27,20 @@ cardScan(function () {
 // cardScan(function () {
 //     console.log("Scan completed!");
 // }, 3000);
+
+// setTimeout(function () {
+//     console.log("It'll take some time")
+// }, 3000);
+
+const numbers = [1, 2, 3, 4, 5];
+function timer(callback, delay) {
+    console.log("calculating...");
+
+    setTimeout(function () {
+        callback()
+    }, delay);
+};
+
+timer(function () {
+console.log(numbers)    
+}, 3000)
